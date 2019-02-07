576182a53bb24777a0d7ea20f4c9a586aa52c8a6
arleybri18 <arleybri18@gmail.com>
2019-02-07 20:21:53 +0000

    program that print the alphabet in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  *
- * Description: This print a alphabet in lowercase negative
+ * Description: This print a alphabet in lowercase and uppercase
  */
 int main(void)
 {
