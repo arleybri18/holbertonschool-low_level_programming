bc5a3cb91826e8d9a9616508923337ac9a64558e
arleybri18 <arleybri18@gmail.com>
2019-02-12 20:58:44 +0000

    add program file to sum two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - returns the sum of two numbers
+ * @a: First parameter
+ * @b: The character to add a number
+ *
+ * Return: On success 0.
+ */
+
+int add(int a, int b)
+{
+	a = a + b;
+	return (a);
+}
