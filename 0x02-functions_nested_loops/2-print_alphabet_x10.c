dd65f83087c8bdc1b35d3c70a50439920352bf94
arleybri18 <arleybri18@gmail.com>
2019-02-12 16:21:45 +0000

    add function that print 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - writes the alphabeth to stdout 1o times
+ * c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (i < 10)
+	{
+		c='a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar(10);
+		i++;
+	}
+}
