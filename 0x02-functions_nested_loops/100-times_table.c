4363604a64f35da5c62c2f3e3313d9c4285527dd
arleybri18 <arleybri18@gmail.com>
2019-02-13 00:45:18 +0000

    program generate tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ * times_table - print table times
+ *
+ * Return: On success 0.
+ */
+
+void times_table(void)
+{
+	int i = 0;
+	int j;
+	int res;
+
+	while (i < 10)
+	{
+		j = 0;
+
+		_putchar(48);
+		while (j < 10)
+		{
+			if (j != 0)
+			{
+				_putchar(44);
+				res = i * j;
+
+				if (res < 10)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar((res % 10) + 48);
+				}
+				else
+				{
+					_putchar(32);
+					_putchar((res / 10) + 48);
+					_putchar((res % 10) + 48);
+				}
+			}
+			j++;
+
+		}
+		_putchar(10);
+		i++;
+	}
+}
