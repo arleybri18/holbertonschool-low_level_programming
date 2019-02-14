c22a401cb64015d32390f562faa6a601a85b7182
arleybri18 <arleybri18@gmail.com>
2019-02-14 17:35:27 +0000

    print numbers 10 times

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - writes the numbers 10 times to stdout
+ * c: The character to print
+ *
+ * Return: On success 1.
+ */
+
+void more_numbers(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		c = 0;
+		while (c <= 14)
+		{
+			if (c < 10)
+			{
+				_putchar(c + 48);
+			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+			c++;
+		}
+		_putchar(10);
+	}
+}
