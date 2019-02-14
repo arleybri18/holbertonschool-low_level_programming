80780954b1cdcac7c0d9db567d75abdf687c837f
arleybri18 <arleybri18@gmail.com>
2019-02-14 15:10:15 +0000

    add program that return a product of two numbers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - returns the product of two numbers
+ * @a: First parameter
+ * @b: The character to multiplicate a number
+ *
+ * Return: On success 0.
+ */
+
+int mul(int a, int b)
+{
+	a = a * b;
+	return (a);
+}
