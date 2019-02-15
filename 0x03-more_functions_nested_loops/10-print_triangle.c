1334657b2becc893dc564a00a9003e16c8c0d8cf
arleybri18 <arleybri18@gmail.com>
2019-02-15 02:51:17 +0000

    print triangle with # character

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - writes the triangle with character #
+ * @size: The side size
+ *
+ * Return: On success 0.
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= (size - i))
+				{
+					_putchar(32);
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
