3ac5fd63d6ab384b7fe4881df55bb25ee3934d17
arleybri18 <arleybri18@gmail.com>
2019-02-15 02:03:34 +0000

    program that print a square of side  length size value

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_square - writes the square with character #
+ * @size: The side size
+ *
+ * Return: On success 0.
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
