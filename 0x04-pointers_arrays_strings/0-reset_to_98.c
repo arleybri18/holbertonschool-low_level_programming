8dfe8f28a5f58266b34c19fc6f67f8f9e8cf2078
arleybri18 <arleybri18@gmail.com>
2019-02-19 16:08:22 +0000

    modify comment in function

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 
 /**
- * void reset_to_98 - change the value of n using a pointer
- * @*n: The variable to change
+ * reset_to_98 - change the value of n using a pointer
+ * @n: The variable to change
  *
- * Return: On success 1.
  */
 
 void reset_to_98(int *n)
