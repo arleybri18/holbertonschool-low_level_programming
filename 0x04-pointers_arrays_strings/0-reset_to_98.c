63a89b6f39229ec49ad023b4e384fd71645f4041
arleybri18 <arleybri18@gmail.com>
2019-02-19 15:52:00 +0000

    program that change value of variable using a pointer

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * void reset_to_98 - change the value of n using a pointer
+ * @*n: The variable to change
+ *
+ * Return: On success 1.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
