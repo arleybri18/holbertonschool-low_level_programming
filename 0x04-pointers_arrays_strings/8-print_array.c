a686eb3cc65619eaa063c37ff2fc1b863fdb46ec
arleybri18 <arleybri18@gmail.com>
2019-02-19 22:01:33 +0000

    add program print values of array

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - print the contain of array
+ * until the position len
+ * @a: receive the string
+ * @n: lenght of array
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		i != (n - 1) ? printf("%d, ", *(a + i)) : printf("%d\n", *(a + i));
+	i++;
+	}
+}
