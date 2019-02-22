ce2f642eae3266a42720d023793960c00cce2cad
arleybri18 <arleybri18@gmail.com>
2019-02-21 20:52:01 -0500

    files task 5

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * rev_string - print the contain of string reverse recorring an array
+ * until the position len
+ * @s: receive the string
+ *
+ */
+
+char *string_toupper(char *)
+{
+	int auxiliar;
+	int i;
+	
+	i = 0;
+	while (i < n)
+	{
+		auxiliar = *(a + n);
+		*(a + n) = *(a + i);
+		*(a + i) = auxiliar;
+		n--;
+		i++;
+	}
+}
