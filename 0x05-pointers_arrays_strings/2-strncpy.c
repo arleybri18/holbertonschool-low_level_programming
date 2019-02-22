47ea06e86e9c64ddebc4f66b472d7b72f3dc4fe6
arleybri18 <arleybri18@gmail.com>
2019-02-22 00:48:45 +0000

    modify file for betty style

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * _strcpy - print the contain of array
- * until the position len
+ * _strncpy - print the contain of array until n numbers
  * @dest: pointer contain a string to return
  * @src: pointer contain a string received
+ * @n: number of characters to copy
  *
  * Return: return char string *dest
  */
@@ -12,14 +12,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	i = 0;
 	while (i < n && src[i] != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
-	for (;i < n; i++)
+	for (; i < n; i++)
 		*(dest + i) = '\0';
 
 	return (dest);
