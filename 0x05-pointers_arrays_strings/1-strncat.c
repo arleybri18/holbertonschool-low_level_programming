0f696ed44a56409c9ea75895634788627eac6ab8
arleybri18 <arleybri18@gmail.com>
2019-02-22 00:40:24 +0000

    modify for betty style

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -33,11 +33,11 @@ char *_strncat(char *dest, char *src, int n)
 	int len_dest = _strlen(dest);
 	int i;
 
-	for(i = 0;i < n && src[i] != '\0';i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[len_dest + i] = src[i];
 	}
 	dest[len_dest + i] = '\0';
 
-	return dest;
+	return (dest);
 }
