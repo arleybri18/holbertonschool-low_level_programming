a3368c4fa8ed2ec0bf58a77054043d159d223421
arleybri18 <arleybri18@gmail.com>
2019-02-22 03:36:38 +0000

    program that change letters by numbers

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * leet - convert string to capitalize
+ * @s: receive the string
+ *
+ * Return: return de string converted
+ */
+
+char *leet(char *s)
+{
+	int i;
+	/**
+	 * Iterate an array and find the letter and change
+	 * letters by numbers
+	 */
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = 4 + 48;
+
+		if (s[i] == 'e' || s[i] == 'E')
+			s[i] = 3 + 48;
+
+		if (s[i] == 'o' || s[i] == 'O')
+			s[i] = 0 + 48;
+
+		if (s[i] == 't' || s[i] == 'T')
+			s[i] = 7 + 48;
+
+		if (s[i] == 'l' || s[i] == 'L')
+			s[i] = 1 + 48;
+		i++;
+	}
+
+	return (s);
+}
