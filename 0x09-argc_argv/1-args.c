e242a1bd91b0a5b00c35ec092d40d3f30e819936
arleybri18 <arleybri18@gmail.com>
2019-03-02 23:42:24 +0000

    program that print number of argumments

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - return the first argument
+ *
+ * @argc: number of arguments
+ * @argv: array of string with the list of arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
