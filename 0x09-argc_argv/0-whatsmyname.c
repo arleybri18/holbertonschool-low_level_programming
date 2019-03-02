2cccc50ba8df2e5e9effef7580220a722d318541
arleybri18 <arleybri18@gmail.com>
2019-03-02 23:06:53 +0000

    program that print firts argument

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - return the first argument
+ *
+ * @argc: number of arguments
+ * @argv: array of string with the list of arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x09-argc_argv/README.md
@@ -0,0 +1,1 @@
+programs using argc y argv
