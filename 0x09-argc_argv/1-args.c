8846dbd977833cc33a3b28e2214848e833cec41f
arleybri18 <arleybri18@gmail.com>
2019-03-03 00:32:36 +0000

    program that count arguments passed

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - return the first argument
@@ -12,6 +13,6 @@
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
