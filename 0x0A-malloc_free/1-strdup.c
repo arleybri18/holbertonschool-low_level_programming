50f2f9baae12469341dbb204013a64e3d028a4c8
arleybri18 <arleybri18@gmail.com>
2019-03-06 00:07:17 +0000

    add function calculate length

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+
+/**
+ * _strlenght - calculate a length of a string
+ *
+ * @s: string
+ *
+ * Return: size of string
+ */
+
+int _strlenght(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+
 /**
  * _strdup - duplicate a string with dynamic array
  *
@@ -12,23 +32,24 @@
 char *_strdup(char *str)
 {
 	int i;
+	int size;
 	char *arr;
-	/**
-	 * using malloc for allocated memory
-	 * malloc return an address to pointer
-	 */
+	/* Validate lenght of array*/
 	if (str[0] == '\0')
 		return (NULL);
-	arr = malloc(sizeof(*str));
+	size = _strlenght(str);
+	if (size == 0)
+		return (NULL);
+
+	/* allocate space char array*/
+	arr = malloc(size * sizeof(char));
 	/* validate return function malloc*/
 	if (arr == NULL)
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; i < size; i++)
 	{
 		arr[i] = str[i];
 	}
-
 	return (arr);
 	free(arr);
 }
