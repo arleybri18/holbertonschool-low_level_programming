13daa004b5ebd1ff0ef12af695187375fe90ed3a
arleybri18 <arleybri18@gmail.com>
2019-03-06 05:13:45 +0000

    free memory program

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -17,7 +17,7 @@ void free_grid(int **grid, int height)
 
 	if (grid == NULL || height == 0)
 		free(grid);
-	for (i = 0; i < height; i++)
+	for (i = 0; i <= height; i++)
 		free(grid[i]);
 	free(grid);
 }
