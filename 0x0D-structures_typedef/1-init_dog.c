f05c1d295d0c16fba8ff36fb4e46c8e115016dbd
arleybri18 <arleybri18@gmail.com>
2019-03-11 22:07:43 +0000

    add stdlib and if statement if *d is NULL

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - function that initialize struct dog
@@ -13,9 +14,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (!d)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 
 }
