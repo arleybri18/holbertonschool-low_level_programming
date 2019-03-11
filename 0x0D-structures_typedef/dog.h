90d938d8b98b9523a5468ae3eb0feb71a2c54af2
arleybri18 <arleybri18@gmail.com>
2019-03-11 20:55:01 +0000

    adjust for betty

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef SCOPE_H
 #define SCOPE_H
 #include "dog.h"
+/**
+  * struct dog -  struct for dog
+  *
+  * @name: name of the dog
+  * @age: number of years of the dog
+  * @owner: name of the owner of the dog
+  */
 
 typedef struct dog
 {
