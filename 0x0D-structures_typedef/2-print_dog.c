648daf7a352e4d5192d9efd8f80d23adacb8fd4d
arleybri18 <arleybri18@gmail.com>
2019-03-11 22:40:30 +0000

    print data of dog struct

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -12,23 +12,21 @@
 void print_dog(struct dog *d)
 {
 	char *new_name;
-	float new_age;
 	char *new_owner;
 
 	if ((*d).name == NULL)
 		new_name = "(nil)";
 	else
 		new_name = (*d).name;
-
-	if ((*d).age == NULL)
-		new_age = "(nil)";
-	else
-		new_age = (*d).age;
 	
 	if ((*d).owner == NULL)
 		new_owner = "(nil)";
 	else
 		new_owner = (*d).owner;
-	
-	printf("Name: %s\nAge: %.6f\nOwner: %s\n", new_name, new_age, new_owner);
+
+	if ((*d).age == 0)
+		printf("Name: %s\nAge: (nil)\nOwner: %s\n", new_name, new_owner);
+	else
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n", new_name, (*d).age, new_owner);
+
 }
