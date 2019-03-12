6fe042f96664da062d15e9530b123338e0cbc1fb
arleybri18 <arleybri18@gmail.com>
2019-03-12 00:38:24 +0000

    add malloc and free to copy the variables

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,58 @@
 #include "dog.h"
 
 /**
+ * _strlenght - calculate a length of a string
+ *
+ * @s: string
+ *
+ * Return: size of string
+ */
+
+unsigned int _strlenght(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+
+/**
+ * _strdup - duplicate a string with dynamic array
+ *
+ * @str: string
+ *
+ * Return: NULL if a size or malloc fail, or copy array it's ok.
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	unsigned int size;
+	char *arr;
+	/* Validate lenght of array*/
+	if (!str)
+		return (NULL);
+	size = 1 + _strlenght(str);
+	if (size == 0)
+		return (NULL);
+
+	/* allocate space char array*/
+	arr = malloc(size * sizeof(char));
+	/* validate return function malloc*/
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i <= size; i++)
+	{
+		arr[i] = str[i];
+	}
+	return (arr);
+	free(arr);
+}
+
+/**
  * new_dog - function that create new dog
  *
  * @name: pointer to name of the dog
@@ -12,23 +64,22 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *new_name;
-	char *new_owner;
-
 	/* define a pointer of type dog_t*/
 	dog_t *my_dog;
 	/* allocate memory for this type*/
 	my_dog = malloc(sizeof(struct dog));
 	if (my_dog == NULL)
 		return (NULL);
-	
-	new_name = malloc(sizeof(*name));
-	if (new_name == NULL)
-		return (NULL);
-
-	new_owner = malloc(sizeof(*owner));
-	if (new_owner == NULL)
-		return (NULL);
+	my_dog->name = _strdup(name);
+	if (my_dog->name == NULL)
+		free(my_dog);
+	my_dog->owner = _strdup(owner);
+	if (my_dog->owner == NULL)
+	{
+		if (my_dog->name == NULL)
+			free(my_dog->name);
+		free(my_dog);
+	}
 
 	/* initialize variables*/
 	my_dog->name = name;
