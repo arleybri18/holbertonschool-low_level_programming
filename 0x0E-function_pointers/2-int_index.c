ae897f44dc90ffd766b4cc393c0b76821d454075
arleybri18 <arleybri18@gmail.com>
2019-03-13 20:49:20 +0000

    add fucntion print index

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,11 +1,27 @@
 #include "function_pointers.h"
 
 /**
- * int_index -
+ * int_index - function that searches for an integer
+ * @array: array with numbers
+ * @size: size of array
+ * @cmp: pointer to function
  *
- * Return: 
+ * Return: integer with index
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		/*validate if function cmp return 0*/
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 
 }
