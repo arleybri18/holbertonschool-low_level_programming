fa1d1b9bb75ee0558afb7bf1cc88105f8eb249a4
arleybri18 <arleybri18@gmail.com>
2019-03-13 19:18:27 +0000

    modify for betty

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name -
+ * print_name - function that prints a name
+ * @name: name to print
+ * @f: pointer to function
  *
- * Return: 
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
