91622b5ea2bc47dbdf2612181cb7a5b1414bceba
arleybri18 <arleybri18@gmail.com>
2019-03-14 01:45:14 +0000

    program opcodes

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print error if argc or argv not allowed
+ *
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Always 0, or error it's fail
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return (0);
+}
