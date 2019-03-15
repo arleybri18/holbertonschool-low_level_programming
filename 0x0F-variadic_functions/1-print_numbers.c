18c626517b721eed207ce14b75fc1f9365953299
arleybri18 <arleybri18@gmail.com>
2019-03-15 20:31:21 +0000

    modify for betty

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -3,6 +3,9 @@
 /**
  * print_numbers - print number in arguments
  *
+ * @separator: string to print between arguments
+ * @n: number of arguments
+ *
  * Return: nothing
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -16,8 +19,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	/* initialize counter*/
 	i = 0;
 	/* Iterate arguments*/
-	do
-	{/* conditions for determinate if print counter*/
+	do {
+		/* conditions for determinate if print counter*/
 		if (i != (n - 1))
 		{
 			if (separator != NULL)
@@ -28,7 +31,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		else
 			printf("%d\n", va_arg(valist, int));
 		i++;
-	}while (i < n);
+	} while (i < n);
 	/* free memory */
 	va_end(valist);
 }
