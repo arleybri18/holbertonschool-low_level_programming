59b29665790fd113789a89b8ca064c0708503770
arleybri18 <arleybri18@gmail.com>
2019-03-17 20:37:45 +0000

    add condition if n > 0

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,19 +14,23 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
-	/* initialize valist for number of arguments*/
-	va_start(valist, n);
-	/* Iterate arguments*/
-	i = 0;
-	while (i < (n - 1))
+	if (n > 0)
 	{
+		/* initialize valist for number of arguments*/
+		va_start(valist, n);
+		/* Iterate arguments*/
+		i = 0;
+		while (i < (n - 1))
+		{
+			printf("%d", va_arg(valist, int));
+			/*validate if separator is NULL*/
+			if (separator != NULL)
+				printf("%s", separator);
+			i++;
+		}
 		printf("%d", va_arg(valist, int));
-		/*validate if separator is NULL*/
-		if (separator != NULL)
-			printf("%s", separator);
-		i++;
+		/* free memory */
+		va_end(valist);
 	}
-	printf("%d\n", va_arg(valist, int));
-	/* free memory */
-	va_end(valist);
+	printf("\n");
 }
