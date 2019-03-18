7d27e96061cd3c0778cf70092b7480d362149492
arleybri18 <arleybri18@gmail.com>
2019-03-18 02:53:09 +0000

    assembly program

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -6,9 +6,9 @@ main:	            ;tells linker entry point
    mov	ebx,1       ;file descriptor (stdout)
    mov	eax,4       ;system call number (sys_write)
    int	0x80        ;call kernel
-	
    mov	eax,0       ;system call number (sys_exit)
    int	0x80        ;call kernel
 
 section	.data
-msg db 'Hello, Holberton', 0x0a  ;string to be printed
+	msg db 'Hello, Holberton', 0x0a  ;string to be printed
+	size equ $ - msg
