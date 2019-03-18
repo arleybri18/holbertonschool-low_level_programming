e8ee2e2729c43a21227e9734d4fd0432fdad7e00
arleybri18 <arleybri18@gmail.com>
2019-03-18 02:05:25 +0000

    add variadic function.h

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,21 +1,32 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
-/* struct contain type and string*/
+void print_char(va_list);
+void print_int(va_list);
+void print_float(va_list);
+void print_string(va_list);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct print_function - contain type and string
+ *
+ * @type: type of function
+ * @func: fucntion to invoque
+ *
+ */
 typedef struct print_function
 {
 const char * const type;
 void (*func)(va_list);
 } print_function;
 
-
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
