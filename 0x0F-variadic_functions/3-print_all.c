f8d91b92cddc56283c3ee8ec2dd7d7e2824f64c1
arleybri18 <arleybri18@gmail.com>
2019-03-18 03:50:22 +0000

    modify for betty

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -23,7 +23,7 @@ void print_all(const char * const format, ...)
 				printf("%c", va_arg(valist, int));
 				break;
 			case 'i':
-				printf("%i", va_arg(valist, int));
+				printf("%d", va_arg(valist, int));
 				break;
 			case 'f':
 				printf("%f", va_arg(valist, double));
@@ -31,7 +31,10 @@ void print_all(const char * const format, ...)
 			case 's':
 				string = va_arg(valist, char *);
 				if (string == NULL)
+				{
 					printf("%p", string);
+					break;
+				}
 				printf("%s", string);
 				break;
 			default:
