981cd347109e23aabe95394ac9e675cda7493f4b
arleybri18 <arleybri18@gmail.com>
2019-03-22 00:51:51 +0000

    Task print before the main

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * my_printf - print function before the main
+ *
+ * Return: nothing
+ *
+ */
+
+void my_printf(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
