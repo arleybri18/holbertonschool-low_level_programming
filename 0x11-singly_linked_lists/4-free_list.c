9daa99309cd2f021447d4bcaa3dc029a9585fe3a
arleybri18 <arleybri18@gmail.com>
2019-03-22 02:42:51 +0000

    free memory of the list

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_list - free list
+ *
+ * @head: head of the list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	/* declare a current node*/
+	list_t *current_node = head;
+	/* declare auxiliar for the next node*/
+	list_t *next_node;
+
+	while (current_node != NULL)
+	{
+		/* save next node */
+		next_node = current_node->next;
+		/* free memory */
+		free(current_node);
+		/* save current_node */
+		current_node = next_node;
+	}
+	/* dereferenced head*/
+	head = NULL;
+}
