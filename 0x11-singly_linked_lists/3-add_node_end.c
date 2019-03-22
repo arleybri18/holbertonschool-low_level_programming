3e3473780f466451ecfece261876338a92633904
arleybri18 <arleybri18@gmail.com>
2019-03-22 02:07:07 +0000

    add steps in the file

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	/* create a new node and the last pointer*/
+	/* 1- create a new node and the last pointer*/
 	list_t *new;
 	list_t *last = *head;
 	/* duplicate and validate string*/
@@ -24,27 +24,28 @@ list_t *add_node_end(list_t **head, const char *str)
 	/*calculate lenght of a string*/
 	while (new_str[lenght] != '\0')
 		lenght++;
-	/* create a new node */
+	/* 1-  allocate new node */
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	/* insert data in a new node*/
+	/* 2- insert data in a new node*/
 	new->str = new_str;
 	new->len = lenght;
+	/* 3 - this node is going to be a last node*/
 	new->next = NULL;
-	/*If the Linked List is empty, then make the new node as head*/
+	/* 4 - If the Linked List is empty, then make the new node as head*/
 	if (*head == NULL)
 	{
 		*head = new;
 		return (*head);
 	}
-	/*Else traverse till the last node*/
+	/* 5 - Else traverse till the last node*/
 	while (last->next != NULL)
 		last = last->next;
-	/*Change the next of last node*/
+	/* 6 - Change the next of last node*/
 	last->next = new;
 	return (*head);
 
