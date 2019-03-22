a6546e95b7d4855baf5fd6dd1a5e6ea21fe73ac6
arleybri18 <arleybri18@gmail.com>
2019-03-22 02:03:53 +0000

    add steps in the file

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -10,10 +10,11 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	/*1 - create a new node*/
 	list_t *new;
 	char *new_str;
 	int lenght = 0;
-
+	/*duplicate a string*/
 	new_str = strdup(str);
 	/* validate if string is null */
 	if (new_str == NULL)
@@ -22,18 +23,19 @@ list_t *add_node(list_t **head, const char *str)
 	while (new_str[lenght] != '\0')
 		lenght++;
 
-	/* create a new node */
+	/* 1 - create a new node */
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-
+	/* 2 - put in the data */
 	new->str = new_str;
 	new->len = lenght;
+	/* 3 - make next of new node as head*/
 	new->next = *head;
-
+	/* 4 - move the head to point to new node*/
 	*head = new;
 
 	return (*head);
