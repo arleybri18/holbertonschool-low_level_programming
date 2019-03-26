070bfa138783107131b3194ec4c5d35c834aa0a7
arleybri18 <arleybri18@gmail.com>
2019-03-26 23:25:25 +0000

    task 10 delete node

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,13 +20,37 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	/*save point to the head*/
 	current_node = *head;
 	prev_node = *head;
-	while (*head)
+	/*calculate lenght of the list*/
+	while (current_node)
 	{
 		counter_idx++;
-		*head = *head->next;
+		current_node = current_node->next;
 	}
-	/**/
+	/*validate if teh index not exist in the list*/
+	if (counter_idx < index)
+		return (-1);
+	/*return to the head*/
+	current_node = *head;
+	counter_idx = 0;
 
+	if (index == 0)
+	{
+		*head = current_node->next;
+		current_node->next = NULL;
+		free(current_node);
+		return (1);
+	}
+	/*iterate until index*/
+	while (counter_idx < index)
+	{
+		current_node = current_node->next;
+		if (counter_idx != 0)
+			prev_node = prev_node->next;
+		counter_idx++;
+	}
 
-
+	prev_node = current_node->next;
+	current_node->next = NULL;
+	free(current_node);
+	return (1);
 }
