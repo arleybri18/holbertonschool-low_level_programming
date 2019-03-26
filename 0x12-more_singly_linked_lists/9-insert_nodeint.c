13ec662c75569d3903354b1106de14fb1faa3962
arleybri18 <arleybri18@gmail.com>
2019-03-26 22:56:34 +0000

    modify task include case idx = 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * len_list - calculate a lenght of the list
+ *
+ * @head: head of the list
+ * Return: lenght of the list
+ */
+unsigned int len_list(listint_t **head)
+{
+	listint_t *aux_node = *head;
+	unsigned int counter_idx;
+
+	counter_idx = 0;
+	/*validate lenght of the list*/
+	while (aux_node)
+	{
+		counter_idx++;
+		aux_node = aux_node->next;
+	}
+	return (counter_idx);
+}
+
+/**
  * insert_nodeint_at_index - insert a new node
  *
  * @head: head of the list
@@ -11,20 +32,14 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	/*declare pointer to new node and aux node*/
-	listint_t *new_node;
-	listint_t *aux_node;
+	listint_t *new_node, *aux_node;
 	unsigned int counter_idx;
 
 	if (*head == NULL)
 		return (NULL);
 	/*save point to head*/
 	aux_node = *head;
-	/*validate lenght of the list*/
-	while (aux_node)
-	{
-		counter_idx++;
-		aux_node = aux_node->next;
-	}
+	counter_idx = len_list(head);
 	/*validate if the index not exist in the list*/
 	if (counter_idx < idx)
 		return (NULL);
@@ -40,6 +55,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/*iterate before index*/
 	counter_idx = 0;
 	aux_node = *head;
+	if (idx == 0)
+	{
+		*head = new_node;
+		new_node->next = aux_node->next;
+		return (new_node);
+	}
 	while (counter_idx < (idx - 1))
 	{
 		aux_node = aux_node->next;
