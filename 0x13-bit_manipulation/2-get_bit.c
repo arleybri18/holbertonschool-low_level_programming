110e0491d4b617b5bfa45e85e972d90ecdfd7ebb
arleybri18 <arleybri18@gmail.com>
2019-03-28 23:56:20 +0000

    get bit at index

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
 
 /**
- * get_bit -
+ * get_bit - get a bit at the index
  *
- * Return: 
+ * @n: number to convert to binary
+ * @index: index to bit to return
+ * Return:  bit at the index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	return ((n >> (index)) & 1);
 
 }
