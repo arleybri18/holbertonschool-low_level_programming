e3d9fc872321589bf818526456f7b8c3b4d3a3dc
arleybri18 <arleybri18@gmail.com>
2019-03-28 23:38:07 +0000

    program that convert to binary

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
- * print_binary -
+ * print_binary - print a number in binary
  *
- * Return: 
+ * @n: number to convert
+ * Return: Nothing
  */
 void print_binary(unsigned long int n)
 {
-
+	/* validate if n is greather than 1*/
+	if (n > 1)
+	{
+		/*call the recursion*/
+		print_binary(n >> 1);
+		/*print a digit only when finished recursion*/
+		_putchar((n & 1) + 48);
+	}
+	else
+	{
+		/*validate strict n equal to zero*/
+		if (n == 0)
+			_putchar('0');
+		else
+			/*only if n equal to 1*/
+			_putchar((n & 1) + 48);
+	}
 }
---
+++ 0x13-bit_manipulation/test2.c
@@ -0,0 +1,19 @@
+// C program to extract k bits from a given 
+// position. 
+#include <stdio.h> 
+  
+// Function to extract k bits from p position 
+// and returns the extracted value as integer 
+int bitExtracted(int number, int k, int p) 
+{ 
+    return (((1 << k) - 1) & (number >> (p - 1))); 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    int number = 2, k = 2, p = 1; 
+    printf("The extracted number is %d",  
+               bitExtracted(number, k, p)); 
+    return 0; 
+} 
