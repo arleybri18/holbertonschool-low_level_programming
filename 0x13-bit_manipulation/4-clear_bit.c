27e5cd9a04448706f8217a41328dc2d68aa48703
arleybri18 <arleybri18@gmail.com>
2019-03-29 00:56:04 +0000

    clear a bit

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * clear_bit -
+ * clear_bit - clear a bit at index
  *
- * Return: 
+ * @n: number to clear
+ * @index: index to change
+ * Return: 1 if success -1 if fail
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int mask;
 
+	if (n == NULL || (index > (sizeof(*n) * 8)))
+		return (-1);
+	mask = 1;
+	mask = mask << index;
+	*n = *n & ~mask;
+	return (1);
 }
