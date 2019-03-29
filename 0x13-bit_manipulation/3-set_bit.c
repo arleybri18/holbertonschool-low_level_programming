10fff590c51f0ac36f7a336651a02f1d092ab18f
arleybri18 <arleybri18@gmail.com>
2019-03-29 00:49:23 +0000

    set the bit

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
 
 /**
- * set_bit -
+ * set_bit - set a bit to 1 at index
  *
- * Return: 
+ * @n: number to change
+ * @index: index to change
+ * Return: 1 if success -1 if fail
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int mask;
+
+	if (n == NULL || (index > (sizeof(*n) * 8)))
+		return (-1);
+	mask = 1;
+	mask = mask << index;
+	*n = *n | mask;
+	return (1);
+
 
 }
