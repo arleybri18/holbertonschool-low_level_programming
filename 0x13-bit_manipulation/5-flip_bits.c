2e0dfae8cc6d89979fb53685ed19919ab0a8b3bc
arleybri18 <arleybri18@gmail.com>
2019-03-29 01:13:52 +0000

    count bit to convert a number

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -7,5 +7,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int acum;
+	unsigned int xor;
+	
+	xor = n ^ m;
 
+	acum = 0;
+	while (xor != 0)
+	{
+		acum = acum + (xor & 1);
+		xor = xor >> 1;
+	}
+	return (acum);
 }
