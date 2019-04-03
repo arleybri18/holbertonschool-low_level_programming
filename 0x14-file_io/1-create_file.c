ac04da2afba21667b3fec29642a36d922db45c32
arleybri18 <arleybri18@gmail.com>
2019-04-03 04:47:01 +0000

    create function str_len

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
 
 /**
+ * str_len - calculate lenght of string
+ *
+ * @str: string to calculate
+ * Return: length of the string
+ */
+
+int str_len(char *str)
+{
+	int len =0;
+
+	while (str[len])
+		len++;
+	return (len);
+}
+
+
+
+/**
  * create_file - create a file and add text_context
  *
  * @filename: name of the file
@@ -9,12 +27,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_desc, write_file, len_text;
-	
-	/*calculate lenght of the string sended*/
-	len_text = 0;
-	while (text_content[len_text])
-		len_text++;
+	int file_desc, write_file;
 	/*open file*/
 	file_desc = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	/*validate if open file fails*/
@@ -30,7 +43,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	/*write in file*/
-	write_file = write(file_desc, text_content, len_text);
+	write_file = write(file_desc, text_content, str_len(text_content));
 	close(file_desc);
 	/*validate if write file fails*/
 	if (write_file == -1)
