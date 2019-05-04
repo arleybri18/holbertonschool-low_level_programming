5a7b3d74b2014f38fdfe2db09129089a3f6bf151
arleybri18 <arleybri18@gmail.com>
2019-05-04 20:45:42 +0000

    program that insert node at end

---
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ *add_dnodeint_end - add node at the beginning of a list
+ *
+ *@head: head of the list
+ *@n: data for the node at the list
+ *Return: address of the new element
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+	dlistint_t *aux = *head;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+
+	while (aux->next != NULL)
+		aux = aux->next;
+
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = aux;
+	aux->next = new_node;
+
+	return (*head);
+}
