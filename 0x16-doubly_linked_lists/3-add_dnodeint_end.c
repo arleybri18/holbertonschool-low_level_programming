98ff32cda11d983d56618046d74bfe203105d4e5
arleybri18 <arleybri18@gmail.com>
2019-05-04 20:50:43 +0000

    program that insert node at end

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,7 +15,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
 	if (*head == NULL)
 	{
