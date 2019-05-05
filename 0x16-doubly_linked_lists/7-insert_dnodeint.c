08796bebd6c23585b73d6a5d0890d857672d7cd7
arleybri18 <arleybri18@gmail.com>
2019-05-05 14:56:21 +0000

    add aux2 to pointer prev to point to new node

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ unsigned int len_list(dlistint_t **head)
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *aux = *h;
+	dlistint_t *new_node = NULL, *aux = *h, *aux2 = NULL;
 	unsigned int count;
 
 	count = len_list(h);
@@ -60,9 +60,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	count = 0;
 	for (count = 0; count < (idx - 1) && aux->next != NULL; count++)
 		aux = aux->next;
+	aux2 = aux->next->next;
 	new_node->n = n;
 	new_node->next = aux->next;
 	new_node->prev = aux->prev;
 	aux->next = new_node;
+	aux2->prev = new_node;
 	return (new_node);
 }
