f1ed1c4bd58b779c7b975cacfc5cd073a47d8c99
arleybri18 <arleybri18@gmail.com>
2019-05-06 03:10:37 +0000

    validate code n

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,72 +1,56 @@
 #include "lists.h"
-/**
- * len_list - calculate a lenght of the list
- *
- * @head: head of the list
- * Return: lenght of the list
- */
-unsigned int len_list(dlistint_t **head)
-{
-	dlistint_t *aux_node = *head;
-	unsigned int counter_idx;
 
-	counter_idx = 0;
-	/*validate lenght of the list*/
-	while (aux_node)
+/**
+ * create_nodeint - function that creates a node of a structure type
+ *
+ * @new: new node to create
+ *
+ * @n: integer data of the structure
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *create_nodeint(dlistint_t *new, int n)
+{
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
 	{
-		counter_idx++;
-		aux_node = aux_node->next;
+		return (NULL);
 	}
-	return (counter_idx);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
 }
 
 /**
- *insert_dnodeint_at_index - insert node at the index
+ * insert_dnodeint_at_index - insert a new node at a given position
  *
- *@h: head of the list
- *@idx: index in the list
- *@n: data for the node at the list
- *Return: address of the new element
+ * @h: head element of a list
+ *
+ * @idx: index of the node to insert
+ *
+ * @n: integer element of the node
+ *
+ * Return: address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *aux = *h;
-	unsigned int count;
+	dlistint_t *new = NULL, *ins, *aux;
+	unsigned int i;
 
-	count = len_list(h);
-	if (count < idx)
-		return (NULL);
-	if (idx == 0)
+	new = create_nodeint(new, n);
+	ins = *h;
+	for (i = 0; i < idx; i++)
 	{
-		aux = add_dnodeint(h, n);
-		return (aux);
+		ins = ins->next;
+		if (ins == NULL)
+			return (NULL);
 	}
-	if (idx == count)
-	{
-		aux = add_dnodeint_end(h, n);
-		return (aux);
-	}
-	if (h == NULL)
-	{
-		new_node->n = n;
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		return (new_node);
-	}
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	count = 0;
-	for (count = 0; count < (idx - 1) && aux->next != NULL; count++)
-		aux = aux->next;
-	new_node->n = n;
-	new_node->next = aux->next;
-	new_node->prev = aux;
-	aux->next = new_node;
-	aux->next->prev = new_node;
-	return (new_node);
+
+	aux = ins->prev;
+	ins->prev = new;
+	aux->next = new;
+	new->next = ins;
+	new->prev = aux;
+	return (new);
 }
