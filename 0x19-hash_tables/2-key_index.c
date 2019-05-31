dc8889c2788ccc1cd503caa133da8ef6bed938ef
arleybri18 <arleybri18@gmail.com>
2019-05-31 03:16:20 +0000

    function that set the values in the hash table

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,12 +1,19 @@
 #include "hash_tables.h"
-
+/**
+ * key_index - function that return the index for the hash table
+ *
+ * @key: key value to hash function
+ * @size: size of the array in the hash table
+ *
+ * Return: return the index
+ *
+ **/
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int idx;
-	
-	(void)size;
-	if(key == NULL)
+
+	if (key == NULL)
 		return (1);
 
 	idx = hash_djb2(key) % size;
---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - create a linked list node in the hash table
+ *
+ * @ht: hash table
+ * @key: string key to set
+ * @value: string value to set
+ *
+ * Return: 1 if sucess 0 if fails
+ *
+ **/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *new_node;
+
+	idx = key_index((unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->key = (char *)key;
+	new_node->value = (char *)value;
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (1);
+}
