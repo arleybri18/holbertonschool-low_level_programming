e11ce5bb46a5d23e6127790dcda5dba5e1e37374
arleybri18 <arleybri18@gmail.com>
2019-05-31 16:15:14 +0000

    use str_dup to duplicate key and value

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,18 +14,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
 	hash_node_t *new_node;
+	char *key_dup;
+	char *value_dup;
 
 	if (key == NULL)
 		return (0);
-	idx = key_index((unsigned char *)key, ht->size);
+	key_dup = strdup(key);
+	value_dup = strdup(value);
+	idx = key_index((unsigned char *)key_dup, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+	new_node->key = key_dup;
+	new_node->value = value_dup;
 	new_node->next = ht->array[idx];
 	ht->array[idx] = new_node;
 	return (1);
