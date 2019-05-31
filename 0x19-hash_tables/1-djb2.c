55d431955cae03386114b567643029c7ffd77619
arleybri18 <arleybri18@gmail.com>
2019-05-31 00:05:57 +0000

    algorithm djb2 implementation

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+/**
+ * hash_djb2 - algorithm of hash djb2
+ *
+ * @str: string for calculate hash
+ *
+ * Return: hash value
+ *
+ **/
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
