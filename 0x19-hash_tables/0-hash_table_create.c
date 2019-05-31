0f2dea646c1ef4a8a714886116cbfb6584822794
arleybri18 <arleybri18@gmail.com>
2019-05-31 15:35:20 +0000

    adjust malloc for create hash_table and node

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -11,14 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 
 	/*create a new hash table*/
-	new_table = malloc(sizeof(hash_table_t *));
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
 	/*set the values*/
 	new_table->size = size;
 	/*allocate space for array of pointers*/
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	new_table->array = malloc(sizeof(hash_node_t) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table->array);
