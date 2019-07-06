e406f4ace7a372cea1436b3417e8002a3e1a6946
arleybri18 <arleybri18@gmail.com>
2019-07-06 01:25:49 +0000

    calculate perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 def island_perimeter(grid):
     width = []
+    height = []
     for x in grid:
         width.append(sum(x))
     w = max(width)
-    height = []
     for x in range(len(grid[0])):
         h = 0
         for y in range(len(grid)):
-            h = h + grid[y][x]
+            print("grid[y][x] es ", grid[y][x])
+            if grid[y][x] is 1:
+                h =+ 1
         height.append(h)
     #print("width es ", width)
     #print("height es ", height)
-    h = max(height)
+    h = sum(height)
     per = 2 * (h + w)
     return per
