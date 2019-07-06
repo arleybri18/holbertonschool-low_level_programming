625e0c4d6f5f04688b98d46348174476607eef53
arleybri18 <arleybri18@gmail.com>
2019-07-06 02:20:20 +0000

    validate if is 1

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 def island_perimeter(grid):
     width = []
-    for x in grid:
-        width.append(sum(x))
-    w = max(width)
     height = []
+    for x in range(len(grid)):
+        w = 0
+        for y in range(len(grid[0])):
+            if grid[x][y] is 1:
+                w = w + grid[x][y]
+        width.append(w)
     for x in range(len(grid[0])):
         h = 0
         for y in range(len(grid)):
             if grid[y][x] is 1:
                 h = h + grid[y][x]
         height.append(h)
-    #print("width es ", width)
-    #print("height es ", height)
     h = max(height)
+    w = max(width)
     per = 2 * (h + w)
     return per
