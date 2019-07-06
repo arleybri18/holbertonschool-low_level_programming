26d51a4d14524650d6c8535ce556f6413860f5e5
arleybri18 <arleybri18@gmail.com>
2019-07-06 02:29:37 +0000

    add comments

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+Function Island perimeter
+"""
+
+
 def island_perimeter(grid):
+    """
+    Argument is a grid
+    Sum side x  row by row and find the max value
+    Sum side y column by column and find the max value
+    Return the parameter
+    """
     width = []
     height = []
     for x in range(len(grid)):
